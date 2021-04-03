7f72395e4134883cef6c25d4e9b7ab077b20a2fd
Sjoerd Vink <sjoerdvink@icloud.com>
2021-04-03 11:43:12 +0200

    getServerSideProps

---
+++ components/ChatScreen.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function ChatScreen() {
+  return (
+    <Container>
+      <h1>a</h1>
+    </Container>
+  );
+}
+
+export default ChatScreen;
+
+const Container = styled.div``;
---
+++ pages/chat/[id].js
@@ -0,0 +1,68 @@
+import Head from "next/head";
+import Sidebar from "../../components/Sidebar";
+import ChatScreen from "../../components/ChatScreen";
+import styled from "styled-components";
+import { db } from "../../firebase";
+
+function Chat({ chat, messages }) {
+  return (
+    <Container>
+      <Head>
+        <title>Chat</title>
+      </Head>
+      <Sidebar />
+      <ChatContainer>
+        <ChatScreen />
+      </ChatContainer>
+    </Container>
+  );
+}
+
+export default Chat;
+
+export async function getServerSideProps(context) {
+  const ref = db.collection("chats").doc(context.query.id);
+  const messagesRes = await ref
+    .collection("messages")
+    .orderBy("timestamp", "asc")
+    .get();
+
+  const messages = messagesRes.docs
+    .map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }))
+    .map((messages) => ({
+      ...messages,
+      timestamp: messages.timestamp.toDate().getTime(),
+    }));
+
+  const chatRes = await ref.get();
+  const chat = {
+    id: chatRes.id,
+    ...chatRes.data(),
+  };
+
+  return {
+    props: {
+      messages: JSON.stringify(messages),
+      chat: chat,
+    },
+  };
+}
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const ChatContainer = styled.div`
+  flex: 1;
+  overflow: scroll;
+  height: 100vh;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+`;
